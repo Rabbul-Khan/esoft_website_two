f7da5cc62e4963019840b23e0218c5ea2bbaa489
Rabbul-Khan <mohammed.rabbul.hossain.khan@g.bracu.ac.bd>
2024-05-16 16:20:42 +0600

    Fix typo in vite config file.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/ esoft_website_two',
+  base: '/esoft_website_two',
   plugins: [react()],
 });
